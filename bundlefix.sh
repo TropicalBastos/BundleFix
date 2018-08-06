31073069377fcf278086128f0bd3642dcaa34bf8
Ian Bastos <ian-bastos@live.com>
2018-08-06 23:52:43 +0100

    added a builder

---
+++ bundlefix.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+###### TURN MINIFY SETTING OFF, THIS IS DONE MANUALLY #########
+php bin/magento config:set dev/js/minify_files 0
+php bin/php bin/magento config:set dev/js/merge_files 1
+
+###### REMOVE STATIC AND ASSETS DEPLOY THE STATIC FILES #######
+rm -r pub/static
+php bin/magento setup:static-content:deploy
+
+###### MINIFY THE MERGED CACHED JS AND REDEPLOY ###############
+gulp bundlefix --merged
+php bin/magento cache:flush
+php bin/magento setup:static-content:deploy
