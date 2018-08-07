b474d4b692f8be685400ada00a4f07e53d8a218f
Ian Bastos <ian-bastos@live.com>
2018-08-07 23:03:17 +0100

    minify html added to magento sh

--- bundlefix.sh
+++ bundlefix.sh
@@ -3,6 +3,7 @@
 ###### TURN MINIFY SETTING OFF, THIS IS DONE MANUALLY #########
 php bin/magento config:set dev/js/minify_files 0
 php bin/php bin/magento config:set dev/js/merge_files 1
+php bin/magento config:set dev/template/minify_html 1
 
 ###### REMOVE STATIC AND ASSETS DEPLOY THE STATIC FILES #######
 rm -r pub/static
